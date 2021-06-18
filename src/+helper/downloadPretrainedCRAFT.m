ff86d33796d164ed74bad184a30921c5c7789037
Anshika Chaurasia <85917434+anshika-MW@users.noreply.github.com>
2021-06-18 12:25:13 +0530

    Add files via upload

--- src/+helper/downloadPretrainedCRAFT.m
+++ src/+helper/downloadPretrainedCRAFT.m
@@ -1,5 +1,5 @@
 function model = downloadPretrainedCRAFT()
-% The downloadPretrainedEfficientDetD0 function downloads the pretrained
+% The downloadPretrainedCRAFT function downloads the pretrained
 % CRAFT network.
 %
 % Copyright 2021 The MathWorks, Inc.
@@ -12,14 +12,14 @@ netFileFullPath = fullfile(dataPath, modelName);
 netFileFull = [netFileFullPath,'.zip'];
 
 if ~exist(netFileFull,'file')
-    fprintf(['Downloading pretrained', modelName ,'network.\n']);
+    fprintf(['Downloading pretrained ', modelName ,' network.\n']);
     fprintf('This can take several minutes to download...\n');
     url = 'https://ssd.mathworks.com/supportfiles/vision/deeplearning/models/TextDetection/craftModel.zip';
     websave (netFileFullPath,url);
     unzip(netFileFullPath, dataPath);
     model = load([dataPath, '/craftNet.mat']);
 else
-    fprintf('Pretrained EfficientDet-D0 network already exists.\n\n');
+    fprintf('Pretrained CRAFT network already exists.\n\n');
     unzip(netFileFullPath, dataPath);
     model = load([dataPath, '/craftNet.mat']);
 end
